e9697935264518faaff68707e53d902ed9e464cf
kekeangel <kekeangel@naver.com>
2015-06-11 19:34:48 +0900

    bugFIX2

--- WinProg2/PolyLine.cpp
+++ WinProg2/PolyLine.cpp
@@ -1,9 +1,13 @@
 #include "stdafx.h"
 #include "PolyLine.h"
-
+#include "WinProg2Doc.h"
+#include "WinProg2View.h"
+#include "MainFrm.h"
 
 PolyLine::PolyLine()
 {
+	CWinProg2Doc* pDoc = (CWinProg2Doc*)((CMainFrame*)AfxGetMainWnd())->GetActiveFrame()->GetActiveDocument();
+	mov_select = FALSE;
 }
 
 
@@ -12,16 +16,127 @@ PolyLine::~PolyLine()
 }
 
 void PolyLine::Draw(CDC *pDC){
-	CPen pen(PS_SOLID, 1, m_color);
+	CWinProg2Doc* pDoc = (CWinProg2Doc*)((CMainFrame*)AfxGetMainWnd())->GetActiveFrame()->GetActiveDocument();
+	CWinProg2View* pView = (CWinProg2View*)((CMainFrame*)AfxGetMainWnd())->GetActiveFrame()->GetActiveView();
+	
+	CPen pen(m_pen_type, m_bold, m_color);
+	CPen mov_pen(PS_SOLID, 6, RGB(0, 0, 0));
 	CPen *oldPen = pDC->SelectObject(&pen);
+	CBrush brush(RGB(0, 0, 0));
+
 	POSITION pos = m_points.GetHeadPosition();
 
 	CPoint p1, p2;
+	pDC->SelectObject(&brush);
+	p1 = m_points.GetNext(pos);
+	pDC->SelectObject(&pen);
+
+	while (pos != NULL) {
+	//	pDC->SelectObject(oldPen);
+		p2 = m_points.GetNext(pos);
+
+		pDC->MoveTo(p1);
+		pDC->LineTo(p2);
+
+		p1 = p2;
+	}
+	pDC->SelectObject(oldPen);
 }
 
-void addPoint(CPoint& point){
+void PolyLine::addPoint(BOOL isDel, CPoint& point){
+	if (isDel){
+		m_points.AddTail(point);
+		del_points.RemoveHead();
+	}
+	else{
+		m_points.AddTail(point);
+	}
+	
 }
 
-void setPencil(int nWidth, COLORREF rgbColor){
+void PolyLine::setPencil(Bold bold, UINT pen_type, COLORREF color){
+	m_color = color;
+	m_bold = bold;
+	m_pen_type = pen_type;
+}
+
+void PolyLine::delPoint(){
+	CPoint point;
+
+	point = m_points.GetTail();
+	del_points.AddTail(point);
+	m_points.RemoveTail();
+
+	
+}
+
+BOOL PolyLine::getselectPoint(CPoint top, CPoint bottom){
+	POSITION pos = m_points.GetHeadPosition();
+
+	CPoint p1, p2;
+	p1 = m_points.GetNext(pos);
+
+	while (pos != NULL) {
+		p2 = m_points.GetNext(pos);
+
+		if (p1.x >= top.x && p1.y >= top.y){
+			if (p2.x <= bottom.x && p2.y <= bottom.y){
+				return TRUE;
+			}
+		}
+
+		p1 = p2;
+	}
+	return FALSE;
+}
+
+Select PolyLine::getDrawType(){
+	return m_select;
+}
+
+void PolyLine::setDrawType(Select select){
+	m_select = select;
+}
+
+void PolyLine::getTopBottomPoint(CPoint& top, CPoint& bottom){
+
+	POSITION pos = m_points.GetHeadPosition();
+
+	CPoint p1, p2;
+	p1 = top = m_points.GetNext(pos);
+
+	while (pos != NULL) {
+		p2 = m_points.GetNext(pos);
+
+		p1 = p2;
+	}
+	bottom = p2;
+
+	mov_select = TRUE;
+}
+
+void PolyLine::setMove(){
+	mov_select = FALSE;
+}
+
+void PolyLine::movePoint(int t_x, int t_y, int b_x, int b_y){
+	POSITION pos = m_points.GetHeadPosition();
+	POSITION old_pos = m_points.GetHeadPosition();
+	CPoint p1, p2;
+	
+
+	p1 = m_points.GetNext(pos);
+	while (pos != NULL) {
+		p2 = m_points.GetNext(pos);
+		
+		p1.x -= t_x;
+		p1.y -= t_y;
+		m_points.SetAt(old_pos, p1);
+		p1 = p2;
+		m_points.GetNext(old_pos);
+	}
+	p1.x -= b_x;
+	p1.y -= b_y;
+	m_points.SetAt(old_pos, p1);
 	
 }
--- WinProg2/PolyLine.h
+++ WinProg2/PolyLine.h
@@ -1,16 +1,36 @@
 #pragma once
-class PolyLine
+#include "Object_Draw.h"
+
+class PolyLine : public Object_Draw
 {
 protected:
 	CList<CPoint, CPoint> m_points;
+	CList<CPoint, CPoint&> del_points;
+
+public:
 	COLORREF m_color;
+	Bold m_bold;
+	UINT m_pen_type;
+	Select m_select;
+	BOOL mov_select;
 
 public:
 	PolyLine();
 	virtual ~PolyLine();
 
-	void Draw(CDC* pDc);
+	virtual void Draw(CDC* pDc);
 
-	void addPoint(CPoint& point);
-	void setPencil(int nWidth, COLORREF rgbColor);
+	void addPoint(BOOL isDel, CPoint& point);
+	void getTopBottomPoint(CPoint& top, CPoint& bottom);
+	
+	void setPencil(Bold bold, UINT pen_type, COLORREF color);
+	void setDrawType(Select select);
+	void movePoint(int t_x, int t_y, int b_x, int b_y);
+	
+	void delPoint();
+
+	virtual BOOL getselectPoint(CPoint top, CPoint bottom);
+	virtual Select getDrawType();
+
+	void setMove();
 };
