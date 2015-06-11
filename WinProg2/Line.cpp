ce989de9de6fa469b7a8f0dae2a6cb32ac51738b
jeong jea hoon <wonae789@naver.com>
2015-06-11 18:25:40 +0900

    line check

--- WinProg2/Line.cpp
+++ WinProg2/Line.cpp
@@ -24,3 +24,19 @@ void Line::draw(CDC* cdc)
 	cdc->MoveTo(pointStart);
 	cdc->LineTo(pointEnd);
 }
+/*
+void Line::Set(int left, int top, int right, int bottom)
+{
+pointStart.x = left;
+pointStart.y = top;
+pointEnd.x = right;
+pointEnd.y = bottom;
+}
+
+void Line::draw(CDC* cdc)
+{
+CPen pen(Line_Pattern, Line_Thin, color);
+cdc->SelectObject(&pen);
+cdc->MoveTo(pointStart);
+cdc->LineTo(pointEnd);
+}*/
